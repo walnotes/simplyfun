e7535974d3b6d388e34e02976daf6ccf53633773
Ariel Chait <apchait@gmail.com>
2013-07-13 16:44:44 -0700

    language created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SfsLanguages::Application.config.secret_token = 'c1561870a74f270c6244695f25c250ecc55c5b4a094d6adb61f2a46aa343585f5b64ec16a0140d92f91c83ed6d61f152b0760b47a7aa6ae6e6ab3ae3b3ca699c'
---
+++ db/migrate/20130713234258_create_languages.rb
@@ -0,0 +1,11 @@
+class CreateLanguages < ActiveRecord::Migration
+  def change
+    create_table :languages do |t|
+      t.string :name_english
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
