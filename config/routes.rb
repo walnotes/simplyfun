389b50821c28602fa5d0cd138d0a6e8ad20217e3
Ariel Chait <apchait@gmail.com>
2013-08-15 12:26:12 -0700

    fixed home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ SfsLanguages::Application.routes.draw do
 
 
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
 
   resources :words_subthemes
 
@@ -88,7 +87,7 @@ SfsLanguages::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'welcome#index'
-
+  ActiveAdmin.routes(self)
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
