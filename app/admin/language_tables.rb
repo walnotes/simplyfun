e4a9b022dcdbe86777be9e1fda95b3f4ecf317e7
Ariel Chait <apchait@gmail.com>
2013-08-15 12:34:22 -0700

    removed langauge table

--- app/admin/language_tables.rb
+++
@@ -1,3 +0,0 @@
-ActiveAdmin.register LanguageTable do
-  
-end
--- app/models/language_table.rb
+++
@@ -1,8 +0,0 @@
-class LanguageTable < ActiveRecord::Base
-  attr_accessible :language_id, :subtheme, :subtheme_description, :subtheme_id, :subtheme_image, :subtheme_pdf_flashcards, :subtheme_pdf_worksheet, :theme, :theme_description, :theme_id, :theme_image, :theme_pdf, :word, :word_audio, :word_id, :word_image, :word_pdf
-
-  belongs_to :language
-  belongs_to :theme
-  belongs_to :subtheme
-  belongs_to :word
-end
