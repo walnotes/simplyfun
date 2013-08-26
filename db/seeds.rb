1381b4f76c42602d331947ef86a20e7573410cf7
WKoning <walterchkoning@yahoo.com>
2013-08-26 10:09:30 -0700

    Updated the path to the image for the seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@
 
 
 spanish = Language.create! name_english: 'Spanish', name: 'Español'
-trip_theme = Theme.create! name: 'The Trip', name_english: 'El Viaje', description: 'Going on Vacation!', image: 'http://fivesomnimedia.com/projects/sfs/theme/img/theme_image_trip.jpg', language_id: spanish.id
+trip_theme = Theme.create! name: 'The Trip', name_english: 'El Viaje', description: 'Going on Vacation!', image: 'http://fivesomnimedia.com/projects/sfs/theme/img/theme_image_the_trip.jpg', language_id: spanish.id
 hotel_subtheme = Subtheme.create! name: 'The Hotel', description: 'Where you sleep when on a trip', image: 'http://fivesomnimedia.com/projects/sfs/theme/img/hotel.jpg', video_url: 'https://vimeo.com/61150433', theme_id: trip_theme.id
 Word.create! ([
 	{ name_english: 'bathroom', subtheme_id: hotel_subtheme.id },
