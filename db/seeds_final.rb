8189494cbcc798ff25a84ae598ac66adad55e454
Ariel Chait <apchait@gmail.com>
2014-02-09 15:29:09 -0800

    added a seedsfinal.rb to loop through csvs

---
+++ db/seeds_final.rb
@@ -0,0 +1,23 @@
+require 'csv'
+
+lang_file = "languages.csv"
+theme_file = "themes.csv"
+subtheme_file = "subthemes.csv"
+word_file = "words.csv"
+
+# Create the languages
+CSV.foreach(Rails.root.join(lang_file), headers: true) do |row|
+	Language.create! name: row[], name_english[] etc...
+end
+
+CSV.foreach(Rails.root.join(theme_file), headers: true) do |row|
+	Theme.create! name: name[], language_id: Language.find_by_name(row[])...
+end
+
+CSV.foreach(Rails.root.join(subtheme_file), headers: true) do |row|
+	Subtheme.create! name: name[], subtheme_id: Subtheme.find_by_name(row[])...
+end
+
+CSV.foreach(Rails.root.join(word_file), headers: true) do |row|
+	Theme.create! name: name[], subtheme_id: Subhtme.find_by_name(row[])...
+end
